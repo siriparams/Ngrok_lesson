eed32ec7c3a25e113d9d65c6fbfbf8378425bd0b
siriparams <siriparamathma06@gmail.com>
2026-02-19 14:20:23 +0530

    1

--- db.py
+++ db.py
@@ -8,7 +8,9 @@ load_dotenv()
 
 Base=declarative_base()
 
-DATABASE_URL=os.getenv("DATABASE_URL")
+DATABASE_URL="sqlite:///./test.db"
+
+DATABASE_URL=os.getenv("DATABASE_URL",DATABASE_URL)
 print("DATABASE_URL",DATABASE_URL)
 
 engine=create_engine(DATABASE_URL)
